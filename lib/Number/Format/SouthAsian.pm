0bc0253872b41ea14feb8576297ce580cb9b5818
Alex Balhatchet <kaoru@slackwise.net>
2010-11-12 16:00:39 +0000

    initial commit - POD only module, compilation test, dist.ini, empty Changes file

---
+++ lib/Number/Format/SouthAsian.pm
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+
+package Number::Format::SouthAsian;
+
+=head1 NAME
+
+Number::Format::SouthAsian - format numbers in the South Asian style
+
+=head1 SYNOPSIS
+
+TODO
+
+=head1 METHODS
+
+TODO
+
+=cut
+
+# methods go here...
+
+=head1 Copyright
+
+Copyright (C) 2009 Lokku Ltd.
+
+=head1 Author
+
+Alex Balhatchet (alex@lokku.com)
+
+=cut
+
+1;
